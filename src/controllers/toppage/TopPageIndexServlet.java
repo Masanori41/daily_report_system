b80afc0482352e34be69dab0241bbec56245f19c
masanori.murakami <kenji.gs.grand@gmail.com>
2019-11-11 14:07:34 +0900

    Modify top page

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -1,13 +1,19 @@
 package controllers.toppage;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.Report;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class TopPageIndexServlet
@@ -29,6 +35,32 @@ public class TopPageIndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        }catch(NumberFormatException ex){
+            page = 1;
+        }
+
+        // ログインした従業員取得
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+
+        List<Report> reports = em.createNamedQuery("getMyAllReports", Report.class)
+                                            .setParameter("employee", login_employee)
+                                            .setFirstResult((page - 1) * 15)
+                                            .setMaxResults(15)
+                                            .getResultList();
+
+        long reports_count = (long)em.createNamedQuery("getMyReportsCount", Long.class)
+                                            .setParameter("employee", login_employee)
+                                            .getSingleResult();
+        em.close();
+
+        request.setAttribute("reports", reports);
+        request.setAttribute("reports_count", reports_count);
+        request.setAttribute("page", page);
         if(request.getSession().getAttribute("flush") != null){
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
@@ -37,5 +69,4 @@ public class TopPageIndexServlet extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
         rd.forward(request, response);
     }
-
 }
