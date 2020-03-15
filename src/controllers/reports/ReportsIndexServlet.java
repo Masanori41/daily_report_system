6c1ba7b4923c26ad4635dcf59eaa7a57e432cb88
masanori.murakami <kenji.gs.grand@gmail.com>
2020-03-15 10:38:43 +0900

    add comment

--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -21,6 +21,7 @@ import utils.DBUtil;
 public class ReportsIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    // testcomment
     /**
      * @see HttpServlet#HttpServlet()
      */
